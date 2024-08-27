bd6b97357a65306309c77900a397288a8a6ab801
akash@maticui <akash@maticui.com>
2024-08-27 12:57:46 +0530

    database query fix

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../model/Todo")
 
 exports.createTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "createTodo success" })
+    await Todo.create(req.body)
+    res.json({ message: "create Todo success" })
 })
 exports.readTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "readTodo success" })
+    const result = await Todo.find()
+    res.json({ message: "read Todo success", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "updateTodo success" })
+    await Todo.findByIdAndUpdate(req.params.id, req.body)
+    res.json({ message: "update Todo success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "deleteTodo success" })
+    await Todo.findByIdAndDelete(req.params.id)
+    res.json({ message: "delete Todo success" })
 })
